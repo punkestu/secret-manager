729cc5815edff2e7aaf95c7e66e0ec09d145579e
punkestu <pangestubima89@gmail.com>
2024-09-10 00:33:07 +0700

    fix path

--- utils/getenv.js
+++ utils/getenv.js
@@ -1,5 +1,7 @@
 export default {
-    "caesarshift": process.env.PERSONALSECRET_CAESAR_SHIFT || 5,
-    "processor": process.env.PERSONALSECRET_PROCESSOR || 0b00000001,
-    "sqlite-filepath": process.env.PERSONALSECRET_SQLITE_FILEPATH || "~/.config/secret-man/sqlite.db",
-}
+  caesarshift: process.env.PERSONALSECRET_CAESAR_SHIFT || 5,
+  processor: process.env.PERSONALSECRET_PROCESSOR || 0b00000001,
+  "sqlite-filepath":
+    process.env.PERSONALSECRET_SQLITE_FILEPATH ||
+    `${process.env.HOME}/.config/secret-man/sqlite.db`,
+};
